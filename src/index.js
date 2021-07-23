613cd098abe89318cb06e421fd1fc2e9ba0b641b
Salustiano Robles Teran <saluroblesteran@gmail.com>
2021-07-23 11:32:42 -0300

    Agregamos boton para mutear el video

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import mediaPlayer from './mediaPlayer';
+import mediaPlayer from './mediaPlayer.js';
+import autoPlay from './plugins/autoPlay.js';
 
 const video = document.querySelector('video');
-const player = new mediaPlayer({el: video});
+const player = new mediaPlayer({ el: video, plugins: [new autoPlay()] });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const button1 = document.querySelector('#pp');
+const button2 = document.querySelector('#mu');
+
+button1.onclick = () => player.togglePlay();
+button2.onclick = () => player.toggleMute();
--- src/mediaPlayer.js
+++ src/mediaPlayer.js
@@ -2,6 +2,16 @@
 
 function mediaPlayer (config) {
     this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    
+    this._initPlugins();
+}
+
+mediaPlayer.prototype._initPlugins = function () {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
 }
 
 mediaPlayer.prototype.play = function () {
@@ -20,6 +30,22 @@ mediaPlayer.prototype.togglePlay = function() {
     this.pause();
   }
 };
+
+mediaPlayer.prototype.toggleMute = function() {
+  if (this.media.muted == false) {
+    this.mute();
+  } else {
+    this.unmute();
+  }
+};
+
+mediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+}
+
+mediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+}
 
 
 export default mediaPlayer;
